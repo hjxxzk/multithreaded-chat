e69c90bfd4c3a408ad77d7c0d8cce1c559ae98dc
agnieszka <273017@student.pwr.edu.pl>
2025-05-20 19:39:34 +0200

    update pylint.yml

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,10 @@
+"""Server module for a simple Flask web application."""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """Returns Hello World."""
     return "<p>Hello, World!</p>"
