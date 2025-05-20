57953ff19b526faa80876d6e776cb2784115fbb0
agnieszka <273017@student.pwr.edu.pl>
2025-05-20 22:05:58 +0200

    add color palette

---
+++ frontend/src/constants/Colors.ts
@@ -0,0 +1,13 @@
+const background = "rgb(221, 202, 217)";
+const primary = "rgb(209, 177, 203)";
+const secondary = "rgb(229, 233, 236)";
+const accentDark = "rgb(124, 97, 108)";
+const accentLight = "rgb(234, 253, 248)";
+
+export default {
+    background,
+    primary,
+    secondary,
+    accentDark,
+    accentLight,
+}
