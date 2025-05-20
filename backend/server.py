ed7e75cf45e9fa3dc2278df4ca2eaa4687f35f02
agnieszka <273017@student.pwr.edu.pl>
2025-05-20 21:57:18 +0200

    init frontend

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,15 @@
 """Server module for a simple Flask web application."""
 
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def hello_world():
     """Returns Hello World."""
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    socketio.run(app, host="localhost", port=8080)
